7e646a7b83e0bbb187d2b2e08d0dbb1ad0f7b3d2
Decha <dechasaejara@gmail.com>
2024-06-02 14:43:05 +0300

    update

--- bot.ts
+++ bot.ts
@@ -4,21 +4,32 @@ export const bot = new Bot("7377106182:AAGOS0_xvyFiG8eU8zTtb6Cde3hjB41Z7Wc"); //
 // Build a custom keyboard:
 const keyboard = new Keyboard()
     .text('Join our community').text('Invite Friends').row()
-    .text('Register').text('Back')
+    .text('Register and get 300 Birr reward')
 
 
 
 bot.command("start", async (ctx) => {
+    console.log(ctx);
+    const user = ctx.message?.from.first_name; // the message object
     // Send the menu.
-    await ctx.reply("Welcome to our bot", { reply_markup: keyboard });
+    await ctx.reply(`Welcome to SmartFx ${user}`, { reply_markup: keyboard });
 });
 
 bot.on(":text", async (ctx) => {
     console.log(ctx);
+    const user = ctx.message?.from.first_name; //
     const message = ctx.message; // the message object
     if (!message) return;
-    if (message.text === "Join our community") return await ctx.reply("Click here to join our main channel:    https://t.me/davidosod");
 
+    if (message.text === "Join our community") {
+        return await ctx.reply("Click here to join our main channel:    https://t.me/davidosod");
+    }
+    if (message.text === "Invite Friends") {
+        return await ctx.reply("Click here to invite your friends:    https://t.me/davidosod");
+    }
+    if (message.text === "Register and get 300 Birr reward") {
+        return await ctx.reply("Enter your bank account to get 300 Birr reward");
+    }
 });
 
 bot.start();
