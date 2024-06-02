bfbc41aefdf0057792d3c4acd9a350bf65da5602
Decha <dechasaejara@gmail.com>
2024-06-02 12:02:17 +0300

    update

--- server.ts
+++ server.ts
@@ -1,9 +1,39 @@
 import { serve } from "https://deno.land/std@0.154.0/http/server.ts";
-import { bot } from "../smartfx/bot.ts";
+
+import { Bot } from "https://deno.land/x/grammy@v1.24.0/mod.ts";
 import { webhookCallback } from "../smartfx/deps.deno.ts";
 
-const handleUpdate = webhookCallback(bot, "std/http");
 
+// Create an instance of the `Bot` class and pass your bot token to it. 
+ export const bot = new Bot("7013563671:AAEPqPGu-J_GVdthbiJDWdxWopPUyxAQAvE"); // <-- put your bot token between the ""
+
+// You can now register listeners on your bot object `bot`.
+// grammY will call the listeners when users send messages to your bot.
+
+// Handle the /start command.
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+// Handle other messages.
+bot.on("message", (ctx) => ctx.reply("Got another message!"));
+
+// Now that you specified how to handle messages, you can start your bot.
+// This will connect to the Telegram servers and wait for messages.
+
+// Start the bot.
+bot.start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+const handleUpdate = webhookCallback(bot, "std/http");
 serve(async (req) => {
   if (req.method == "POST") {
     const url = new URL(req.url);
