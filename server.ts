bb8eb03b111edc27eade35f7cfd5002fe837e2b8
Decha <dechasaejara@gmail.com>
2024-06-02 12:26:04 +0300

    update

--- server.ts
+++ server.ts
@@ -1,36 +1,21 @@
 
-import { Bot } from "https://deno.land/x/grammy@v1.24.0/mod.ts";
-
-// Create an instance of the `Bot` class and pass your bot token to it. 
- export const bot = new Bot("7013563671:AAEPqPGu-J_GVdthbiJDWdxWopPUyxAQAvE"); // <-- put your bot token between the ""
-
-// You can now register listeners on your bot object `bot`.
-// grammY will call the listeners when users send messages to your bot.
-
-// Handle the /start command.
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-// Handle other messages.
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
-
-// Now that you specified how to handle messages, you can start your bot.
-// This will connect to the Telegram servers and wait for messages.
-
-// Start the bot.
-bot.start();
+import { webhookCallback } from "https://deno.land/x/grammy@v1.24.0/mod.ts";
+import { bot } from "./bot.ts";
+// You might modify this to the correct way to import your `Bot` object.
 
 
+const handleUpdate = webhookCallback(bot, "std/http");
 
-
-
-
-
-
-
-const port = 8080;
-const handler = (request: Request): Response => {
-  const body = `Hello from Deno server!`;
-  return new Response(body, { status: 200 });
-};
-
-console.log(`HTTP server running. Access it at: http://localhost:${port}/`);
-Deno.serve({ port }, handler);
+Deno.serve(async (req) => {
+  if (req.method === "POST") {
+    const url = new URL(req.url);
+    if (url.pathname.slice(1) === bot.token) {
+      try {
+        return await handleUpdate(req);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+  return new Response();
+});
