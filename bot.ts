f445fc11a5c3fd39d3ecd9bf92bcb5b4450e155b
Decha <dechasaejara@gmail.com>
2024-06-02 12:39:31 +0300

    add ready

--- bot.ts
+++ bot.ts
@@ -1,13 +1,8 @@
 import { Bot } from "https://deno.land/x/grammy@v1.24.0/mod.ts";
 
-// Create an instance of the `Bot` class and pass your bot token to it. 
 export const bot = new Bot("7013563671:AAEPqPGu-J_GVdthbiJDWdxWopPUyxAQAvE"); // <-- put your bot token between the ""
 
-// You can now register listeners on your bot object `bot`.
-// grammY will call the listeners when users send messages to your bot.
-
-// Handle the /start command.
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-// Handle other messages.
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
+bot.command("ready", (ctx) => ctx.reply("Hello!"));
+
 bot.start()
