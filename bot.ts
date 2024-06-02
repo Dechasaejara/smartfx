f1d5feb92f88c862de6b3845a709199b8667222d
Decha <dechasaejara@gmail.com>
2024-06-02 14:07:14 +0300

    update bot

--- bot.ts
+++ bot.ts
@@ -1,6 +1,4 @@
 import { Bot, Keyboard } from "https://deno.land/x/grammy@v1.24.0/mod.ts";
-import { Menu } from "https://deno.land/x/grammy_menu@v1.2.1/mod.ts";
-
 export const bot = new Bot("7377106182:AAGOS0_xvyFiG8eU8zTtb6Cde3hjB41Z7Wc"); // old(aron) => 7013563671:AAEPqPGu-J_GVdthbiJDWdxWopPUyxAQAvE
 
 // Build a custom keyboard:
@@ -8,19 +6,19 @@ const keyboard = new Keyboard()
     .text('Join our community').text('Invite Friends').row()
     .text('Register').text('Back')
 
-bot.on(":text", async (ctx) => {
-    // console.log(ctx);
-    const message = ctx.message; // the message object
-    if (!message) return;
-    if (message.text === "Join our community") return await ctx.reply("Click here to join our main channel: https://t.me/davidosod");
-
-});
-
 
 
 bot.command("start", async (ctx) => {
     // Send the menu.
     await ctx.reply("Welcome to our bot", { reply_markup: keyboard });
+});
+
+bot.on(":text", async (ctx) => {
+    console.log(ctx);
+    const message = ctx.message; // the message object
+    if (!message) return;
+    if (message.text === "Join our community") return await ctx.reply("Click here to join our main channel:    https://t.me/davidosod");
+
 });
 
 bot.start();
