35e28c840c64fe4a5f5d55632f29f4b6904f3cdf
Decha <dechasaejara@gmail.com>
2024-06-18 14:23:36 +0300

    update

--- mod.ts
+++ mod.ts
@@ -1,61 +1,39 @@
 import { Bot, Keyboard } from "https://deno.land/x/grammy@v1.24.0/mod.ts";
-import { serve } from "https://deno.land/std@0.178.0/http/server.ts";
-
-const botToken = "7377106182:AAGOS0_xvyFiG8eU8zTtb6Cde3hjB41Z7Wc";
-const port = 8000;
-
-const bot = new Bot(botToken);
+export const bot = new Bot("7377106182:AAGOS0_xvyFiG8eU8zTtb6Cde3hjB41Z7Wc"); // old(aron) => 7013563671:AAEPqPGu-J_GVdthbiJDWdxWopPUyxAQAvE
 
 // Build a custom keyboard:
 const keyboard = new Keyboard()
-  .text("Join our community")
-  .text("Invite Friends")
-  .row()
-  .text("Register and get 300 Birr reward");
+    .text('Join our community').text('Invite Friends').row()
+    .text('Register and get 300 Birr reward')
+
+
 
 bot.command("start", async (ctx) => {
-  const user = ctx.message?.from.first_name; // the message object
-  // Send the menu.
-  await ctx.reply(`Welcome to SmartFx bot `, { reply_markup: keyboard });
+    const user = ctx.message?.from.first_name; // the message object
+    // Send the menu.
+    await ctx.reply(`Welcome to SmartFx bot `, { reply_markup: keyboard });
 });
 
 bot.on(":text", async (ctx) => {
-  const message = ctx.message; // the message object
-  if (!message) return;
+    const message = ctx.message; // the message object
+    if (!message) return;
 
-  if (message.text === "Join our community") {
-    return await ctx.reply(
-      "Click here to join our main channel:    https://t.me/+5IyRGnhrSvcxNmU0",
-      { reply_markup: keyboard }
-    );
-  }
-  if (message.text === "Invite Friends") {
-    return await ctx.reply(
-      "Click here to invite your friends:    https://t.me/+5IyRGnhrSvcxNmU0",
-      { reply_markup: keyboard }
-    );
-  }
-  if (message.text === "Register and get 300 Birr reward") {
-    return await ctx.reply(
-      "Enter your bank account to get 300 Birr reward",
-      { reply_markup: keyboard }
-    );
-  }
-  if (message.text.length >= 10) {
-    return await ctx.reply("Thankyou to register this bot", { reply_markup: keyboard });
-  } else {
-    return await ctx.reply("Invalid account, please try again!", { reply_markup: keyboard });
-  }
+    if (message.text === "Join our community") {
+        return await ctx.reply("Click here to join our main channel:    https://t.me/+5IyRGnhrSvcxNmU0", { reply_markup: keyboard });
+    }
+    if (message.text === "Invite Friends") {
+        return await ctx.reply("Click here to invite your friends:    https://t.me/+5IyRGnhrSvcxNmU0", { reply_markup: keyboard });
+    }
+    if (message.text === "Register and get 300 Birr reward") {
+
+        return await ctx.reply("Enter your bank account to get 300 Birr reward", { reply_markup: keyboard });
+    }
+    if (message.text.length >= 10) {
+        return await ctx.reply("Thankyou to register this bot", { reply_markup: keyboard });
+    } else {
+        return await ctx.reply("Invalid account, please try again!", { reply_markup: keyboard });
+    }
+
 });
 
-// Start the server
-serve(async (req) => {
-  await bot.handleUpdate(await req.json());
-  return new Response("OK");
-}, { port });
-
-console.log(`Server running on http://localhost:${port}/`);
-
-
-// docker build -t smartfx .
-// docker run -p 8000:8000 smartfx
+bot.start();
